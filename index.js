dc4e4e6a2be43422c70cd185e7fdcbc47d0882c2
karolyiszabolcszoltan-30130-stack <karolyiszabolcszoltan-30130@taszi.hu>
2026-09-16 10:14:01 +0200

    init konyvek

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3030;
+// http://localhost:3030
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({info: "Könyvek backend alkalmazás"})
+});
+
+app.listen(PORT, () => {
+    console.log("Szerver elindult a " + PORT + "-as porton.")
+});
